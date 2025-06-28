1ea4e7b2ba5cabd0720a50bd06553845f52f296f
Luo Sixian <shikenr@outlook.com>
2025-06-28 16:35:31 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const buildConfig = () => {
   const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Life Journey.";
   const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Sixian";
   const defaultTitle =
-    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "SixianBlog";
+    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Sixian";
   const defaultDescription = process.env.NEXT_PUBLIC_BLOG_DESCRIPTION || "Blog about life.";
 
   return {
