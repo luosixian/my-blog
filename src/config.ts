23df2da998120dfd6915c3c1c5a19b32d5fdd9e0
Luo Sixian <shikenr@outlook.com>
2025-06-29 23:32:59 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
   if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
   const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Life Journey.";
-  const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Sixian's Blog";
+  const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Sixian";
   const defaultTitle =
-    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Sixian";
+    process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Sixian's Blog";
   const defaultDescription = process.env.NEXT_PUBLIC_BLOG_DESCRIPTION || "Blog about life.";
 
   return {
