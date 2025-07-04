5d37a097b180be1c2d38a1e5ce9e34e1134a5121
Luo Sixian <shikenr@outlook.com>
2025-07-05 01:46:26 +0900

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ const content = `# About Me
 
 Hi! I'm Sixian. However you ended up on this page, I’m happy you did. Welcome! 
 
-I come from a quiet little town tucked away in **southwest China**, but my passion for anime pulled me across the ocean to **Japan**, where I earned a master’s degree in **computer science**. These days, I live in **Tokyo**, working a job that isn’t always fun, but has its value.
+I come from a quiet little town tucked away in southwest China, but my passion for anime pulled me across the ocean to Japan, where I earned a master’s degree in computer science. These days, I live in Tokyo, working a job that isn’t always fun, but has its value.
 
 Think of this blog less like a “blog” and more like my personal corner of the internet. A little chaotic, a little impulsive, maybe even occasionally insightful—kind of like Weibo/Twitter.
 
@@ -16,9 +16,9 @@ My journey into the anime rabbit hole officially began in **2011** with [緋弾�
 
 Outside of work and anime, I love mobile games. These days it’s mostly [Genshin impact](https://genshin.hoyoverse.com/en/) and a bit of [Star Rail](https://hsr.hoyoverse.com/en-us/). I used to be deep into FGO for six years before I finally quit. I’ve also tried games like Blue Archive, Idolmaster, Azur Lane, Princess Connect, and BanG Dream. 
 
-I also enjoy **PC games** on **Steam**—in 2024 I played *Black Myth: Wukong*, *Sons of the Forest*, and *MiSide*. I *used to* be into **galgames**, and though I don’t play many nowadays, [*BaldrSky*](https://ja.wikipedia.org/wiki/BALDR_SKY) still haunts my heart (in a good way).
+I also enjoy **PC games** on Steam—in 2024 I played *Black Myth: Wukong*, *Sons of the Forest*, and *MiSide*. I *used to* be into **galgames**, and though I don’t play many nowadays, [BaldrSky](https://ja.wikipedia.org/wiki/BALDR_SKY) still haunts my heart (in a good way).
 
-I love **offline events** too! The last convention I went to was [*Wonder Festival Shanghai 2024*](https://wonfes.jp/), and my most recent live was [**Roselia’s「Sei stark」**](https://bang-dream.com/events/roselia2025live)—and let me tell you, being surrounded by people who love what you love? There's nothing quite like it.
+I love offline events too! The last convention I went to was [Wonder Festival Shanghai 2024](https://wonfes.jp/), and my most recent live was [Roselia’s「Sei stark」](https://bang-dream.com/events/roselia2025live)—and let me tell you, being surrounded by people who love what you love? There's nothing quite like it.
 
 Outside of otaku life, I also enjoy **traveling**. Over the past year, I’ve wandered through **Kobe**, **Hiroshima**, **Chengdu**, **Leshan**, **Yibin**, and [Numazu](https://www.city.numazu.shizuoka.jp/). I like driving through the countryside in Japan, just to enjoy the feeling.
 
