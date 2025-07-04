f3e4ba436ab087497a96d5f17016c2391e652e6a
Luo Sixian <shikenr@outlook.com>
2025-07-05 01:49:45 +0900

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,11 +16,11 @@ My journey into the anime rabbit hole officially began in **2011** with [緋弾�
 
 Outside of work and anime, I love mobile games. These days it’s mostly [Genshin impact](https://genshin.hoyoverse.com/en/) and a bit of [Star Rail](https://hsr.hoyoverse.com/en-us/). I used to be deep into FGO for six years before I finally quit. I’ve also tried games like Blue Archive, Idolmaster, Azur Lane, Princess Connect, and BanG Dream. 
 
-I also enjoy **PC games** on Steam—in 2024 I played *Black Myth: Wukong*, *Sons of the Forest*, and *MiSide*. I *used to* be into **galgames**, and though I don’t play many nowadays, [BaldrSky](https://ja.wikipedia.org/wiki/BALDR_SKY) still haunts my heart (in a good way).
+I also enjoy PC game on Steam—in 2024 I played *Black Myth: Wukong*, *Sons of the Forest*, and *MiSide*. I *used to* be into galgames, and though I don’t play many nowadays, [BaldrSky](https://ja.wikipedia.org/wiki/BALDR_SKY) still haunts my heart (in a good way).
 
 I love offline events too! The last convention I went to was [Wonder Festival Shanghai 2024](https://wonfes.jp/), and my most recent live was [Roselia’s「Sei stark」](https://bang-dream.com/events/roselia2025live)—and let me tell you, being surrounded by people who love what you love? There's nothing quite like it.
 
-Outside of otaku life, I also enjoy **traveling**. Over the past year, I’ve wandered through **Kobe**, **Hiroshima**, **Chengdu**, **Leshan**, **Yibin**, and [Numazu](https://www.city.numazu.shizuoka.jp/). I like driving through the countryside in Japan, just to enjoy the feeling.
+Outside of otaku life, I also enjoy **traveling**. Over the past year, I’ve wandered through **Kobe**, **Hiroshima**, **Chengdu**, **Leshan**, **Yibin**, and [**Numazu**](https://www.city.numazu.shizuoka.jp/). I like driving through the countryside in Japan, just to enjoy the feeling.
 
 **人生就是一场旅途** — life is a journey. And if the scenery along the way isn’t worth remembering, then what’s the point of the destination? This blog is my way of recording those little moments—the pixels, the people, the places.
 
