170129a5de7a3213d83460b846ccce118c96847b
Luo Sixian <shikenr@outlook.com>
2025-07-05 14:14:27 +0900

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,11 +14,11 @@ Think of this blog less like a “blog” and more like my personal corner of th
 
 My journey into the anime rabbit hole officially began in **2011** with [緋弾のアリア](https://ja.wikipedia.org/wiki/%E7%B7%8B%E5%BC%BE%E3%81%AE%E3%82%A2%E3%83%AA%E3%82%A2). I've seen a lot of anime since I became an otaku. I don’t watch as much now, but I still follow new shows if they look interesting.
 
-Outside of work and anime, I love mobile games. These days it’s mostly [Genshin impact](https://genshin.hoyoverse.com/en/) and a bit of [Star Rail](https://hsr.hoyoverse.com/en-us/). I used to be deep into FGO for six years before I finally quit. I’ve also tried games like Blue Archive, Idolmaster, Azur Lane, Princess Connect, and BanG Dream. 
+Outside of work and anime, I love mobile games. These days it’s mostly [Genshin impact](https://genshin.hoyoverse.com/en/) and a bit of [Star Rail](https://hsr.hoyoverse.com/en-us/). I used to be deep into [FGO](https://www.fate-go.jp//) for six years before I finally quit. I’ve also tried games like Blue Archive, Idolmaster, Azur Lane, Princess Connect, and BanG Dream. 
 
 I also enjoy PC game on Steam—in 2024 I played *Black Myth: Wukong*, *Sons of the Forest*, and *MiSide*. I *used to* be into galgames, and though I don’t play many nowadays, [BaldrSky](https://ja.wikipedia.org/wiki/BALDR_SKY) still haunts my heart (in a good way).
 
-I love offline events too! The last convention I went to was [Wonder Festival Shanghai 2024](https://wonfes.jp/), and my most recent live was [Roselia’s「Sei stark」](https://bang-dream.com/events/roselia2025live)—and let me tell you, being surrounded by people who love what you love? There's nothing quite like it.
+I love offline events too! The last convention I went to was [Wonder Festival Shanghai 2024](https://wonfes.jp/), and my most recent live was [Roselia’s「Sei stark」](https://bang-dream.com/events/roselia2025live)—being surrounded by people who love what you love? There's nothing quite like it.
 
 Outside of otaku life, I also enjoy traveling. Over the past year, I’ve wandered through Kobe, Hiroshima, Chengdu, Leshan, Yibin, and [Numazu](https://www.city.numazu.shizuoka.jp/). I like driving through the countryside in Japan, just to enjoy the feeling.
 
